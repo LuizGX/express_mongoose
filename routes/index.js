2c8655dd83f00cf44e85629cff9d5fbf583b79ba
LuizGX <luizxvr@hotmail.com>
2019-06-01 14:49:02 -0300

    first commit

--- routes/index.js
+++ routes/index.js
@@ -22,5 +22,21 @@ var router = express.Router();
         });
     });
 
+    
+  /* POST ONE customer. */
+  router.post('/customers/', function (req, res, next) {
+        var db = require('../db');
+        var Customer = db.Mongoose.model('customers', db.CustomerSchema, 'customers');
+        var newcustomer = new Customer({ name: req.body.name, email: req.body.email });
+        newcustomer.save(function (err) {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                res.end();
+                return;
+            }
+            res.json(newcustomer);
+            res.end();
+        });
+    });
 
 module.exports = router;
